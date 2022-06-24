cde8a403588b36bd1596f3b0bc56aa6a79e7700f
jean paul <jeanpaulabizeid@gmail.com>
2022-06-24 15:24:45 +0300

    creating a log in api

--- NodeJs-server/src/user/index.js
+++ NodeJs-server/src/user/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
-const {register} = require('./user');
+const {register, login} = require('./user');
 const router = Router();
 
 router.post('/auth/register', register);
+router.post('/auth/login', login);
 
 module.exports = router;
--- NodeJs-server/src/user/user.js
+++ NodeJs-server/src/user/user.js
@@ -1,6 +1,8 @@
 const User = require('../../model/User');
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 
 async function addUser(body, hashPassword) {
     const {
@@ -15,7 +17,13 @@ async function addUser(body, hashPassword) {
     });
   
     return await user.save();
-  }
+}
+
+async function getByEmail(email) {
+    return await User.findOne({
+      email
+    });
+}
 
 async function register(req, res) {
 try {
@@ -33,4 +41,24 @@ try {
 }
 }
 
-module.exports = {register};
+async function login(req, res) {
+    try {
+      const user = await getByEmail(req.body.email);
+      if (!user) return res.status(400).send('invalid credentials');
+  
+      const validPassword = await bcrypt.compare(req.body.password, user.password);
+      if (!validPassword) return res.status(400).send('invalid credentials');
+  
+      const token = jwt.sign(
+        {_id: user._id, name: user.name, email: user.email},
+        TOKEN_SECRET
+      );
+  
+      return res.header('auth-token', token).send(token).status(200);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+}
+
+module.exports = {register, login};
